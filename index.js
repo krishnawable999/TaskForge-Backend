331780543e2bea243983a0c82f0a8f65851f03e7
Krishna Ranjit Wable <krishnawable999@gmail.com>
2024-05-07 11:00:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import express from "express"
-import morgan from "morgan"
+import express from "express";
+import morgan from "morgan";
 import dbConnection from "./utils/index.js";
 import { errorHandler, routeNotFound } from "./middlewares/errorMiddleware.js";
 
 import routes from "./routes/index.js";
+
 dotenv.config();
 dbConnection();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-const p = process.env.PORT
-//origin to get only req. from the defined ports
-// methos means it can access only request from following methods
+
+// origin to get only requests from the defined ports
+// methods means it can access only requests from the following methods
 app.use(cors({
-    origin: ["p", "p"],
-    methods: ["GET","POST","PUT","DELETE"],
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
+
 app.use("/api", routes);
 
 app.use(routeNotFound);
 app.use(errorHandler);
 
-app.listen(PORT, () =>
-    console.log("Server Started at port"+PORT))
+app.listen(PORT, "0.0.0.0", () =>
+    console.log("Server Started at port " + PORT)
+);
